643226286af0ad94b56601a3aa8f2faf57dd152c
RekhaaChawla <rekha9.chawla@gmail.com>
2025-05-23 19:13:27 +0530

    variable

---
+++ lab05_variable.java
@@ -0,0 +1,11 @@
+public class lab05_variable {
+    public class lab05_variable{
+        public static void main(String[] args) {
+            int a=10;
+            a=65+1;
+            System.out.println(a);
+
+
+
+    }
+}
