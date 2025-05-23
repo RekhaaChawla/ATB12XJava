baeb2a375dcc92978f8347a568f3f57ff30b325c
RekhaaChawla <rekha9.chawla@gmail.com>
2025-05-23 19:16:56 +0530

    variable

---
+++ src/ex_01_Java_Basics/Lab07_variable.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab07_variable {
+    public static void main(String[] args) {
+        int a = 10;
+        a = 65 + 1;
+        System.out.println(a);
+    }
+}
