a2a9a54f263a9601a2dfa0fa9cb692c85c1e783a
RekhaaChawla <rekha9.chawla@gmail.com>
2025-05-25 23:59:20 +0530

    BODMAS

---
+++ src/ex_03_operators/BODMAS.java
@@ -0,0 +1,13 @@
+package ex_03_operators;
+
+public class BODMAS {
+    public static void main(String[] args) {
+        System.out.println((9*3/9+1) *3);
+        //first 9*3 = 27
+        //then 27/9 = 3
+        //after that 3+1=4
+        //in the end 4*3 =12
+        // the answer will be 12
+        //Bracket,order,divide,multiplication,addition and substraction
+    }
+}
